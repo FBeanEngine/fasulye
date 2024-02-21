396c98fa19102d54b36e93d9703df589ddceec44
İbrahim Uysal <74019847+uysalibov@users.noreply.github.com>
2024-02-21 22:59:37 +0300

    update: add owner(player) for gun position

--- include/FGun.h
+++ include/FGun.h
@@ -9,15 +9,18 @@ class FGun : public FObject
 {
 public:
     FGun();
+    ~FGun();
     void Shoot(Vector2 target);
-    void Update(Vector2 position, float dt);
-    void Draw(float dt);
+    void Update(float dt) override;
+    void Draw(float dt) override;
     void SetPosition(Vector2 position);
     void SetRotation(float rotation);
     void SetTexture(Texture2D texture);
     void SetFireRate(int fireRate);
+    void SetOwner(FObject *const owner);
 
 private:
+    FObject *m_owner;
     Vector2 m_position;
     float m_rotation;
     Texture2D m_texture;
--- src/FGun.cpp
+++ src/FGun.cpp
@@ -1,38 +1,54 @@
 #include "FGun.h"
+#include "FSceneManager.h"
+#include "FInputManager.h"
 
 FGun::FGun()
 {
+	TraceLog(LOG_INFO, "FGun::FGun()");
 	m_position = {0, 0};
 	m_rotation = 0;
 	m_fireRate = 1;
 }
 
+FGun::~FGun()
+{
+	TraceLog(LOG_INFO, "FGun::~FGun()");
+}
+
 void FGun::Shoot(Vector2 target)
 {
 	std::unique_ptr<FAmmo> ammo(new FAmmo(m_position, target, 1, 75, 100, LoadTexture("resources/assets/ammo.png")));
-	m_ammos.push_back(std::move(ammo));
+	FSceneManager::AddObjectToActiveScene(std::move(ammo));
 }
 
-void FGun::Update(Vector2 position, float dt)
+void FGun::Update(float dt)
 {
-	m_position = {position.x + 60, position.y + 36};
-	for (size_t i = 0; i < m_ammos.size(); i++)
+	// for (size_t i = 0; i < m_ammos.size(); i++)
+	// {
+	// 	m_ammos[i]->Update(dt);
+	// 	if (m_ammos[i]->isDestroyed)
+	// 	{
+	// 		m_ammos.erase(m_ammos.begin() + i);
+	// 	}
+	// }
+	m_position = m_owner->position;
+	Vector2 movementVector = FInputManager::GetMovementVector();
+	Vector2 aimVector = FInputManager::GetAimVector(m_position);
+	PerformedAction action = FInputManager::GetPerfomedAction();
+	if (action == PerformedAction::LeftHandUse)
 	{
-		m_ammos[i]->Update(dt);
-		if (m_ammos[i]->isDestroyed)
-		{
-			m_ammos.erase(m_ammos.begin() + i);
-		}
+		// animation.BindAnimation("shoot");
+		Shoot(m_mousePosition);
 	}
 }
 
 void FGun::Draw(float dt)
 {
-	DrawTextureEx(m_texture, m_position, m_rotation, 1.5, WHITE);
-	for (size_t i = 0; i < m_ammos.size(); i++)
-	{
-		m_ammos[i]->Draw();
-	}
+	// DrawTextureEx(m_texture, m_position, m_rotation, 5, WHITE);
+	// for (size_t i = 0; i < m_ammos.size(); i++)
+	// {
+	// 	m_ammos[i]->Draw();
+	// }
 }
 
 void FGun::SetPosition(Vector2 position)
@@ -53,4 +69,9 @@ void FGun::SetTexture(Texture2D texture)
 void FGun::SetFireRate(int fireRate)
 {
 	m_fireRate = fireRate;
+}
+
+void FGun::SetOwner(FObject *const owner)
+{
+	m_owner = owner;
 }
